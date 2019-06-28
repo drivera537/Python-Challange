ab0a1069b47c4bd50422f090ffd10ab01161cce9
Darel Rivera <darelrivera@Darels-MBP.home>
2019-06-28 01:51:51 -0400

    updated

--- pyBank/main.py
+++ pyBank/main.py
@@ -2,11 +2,11 @@
 import os
 import csv
 
-#Path collection for the Data
+#Path
 dataCSV = os.path.join('Resources','budget_data.csv')
 dataFinal = os.path.join('Resources','budget_data.txt')
 
-#Initialize variables
+#variables
 monthCount = 0
 total = 0 
 PreValue = 0 
@@ -15,7 +15,7 @@ profitMax = 0
 profitMin = 0
 plist = []
 
-#Open and read CSV file
+#open & read csv file
 with open(dataCSV, newline='') as csvfile:
      csvreader = csv.reader(csvfile, delimiter=',')
      csv_header = next(csvreader)
@@ -25,17 +25,17 @@ with open(dataCSV, newline='') as csvfile:
          iAmount = int(Amount)
          profit =  iAmount - PreValue
          plist = plist + [profit]
-         #Placeholder to track greatest increase in profits (financial analysis)
+         #Track greatest increase in profits 
          if profitMax < profit:
             profitMax = profit
             profitMaxDate = month
-         #Placeholder to track greatest decrease in profits (financial analysis)
+         #track greatest decrease in profits 
          if profitMin > profit:
             profitMin = profit
             profitMinDate = month
 
          PreValue = iAmount   
-         # Get total months (financial analysis)
+         # Get total months 
          monthCount = monthCount + 1
          total += int(Amount)
          
@@ -43,8 +43,8 @@ average  = sum(int(i) for i in plist[1:87])/(monthCount-1)
 average = round(average,2)
 
 final = (
-    f'Financial Analysis\n'
-    f'----------------------------\n' 
+    f'Financial Analysis \n'
+    f'----------------------------\n'
     #total months
     f'Total Months : {monthCount}\n'
     #The total "Profit/Losses" 
